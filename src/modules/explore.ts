a583260ccb2f8054cc92f0b1ae35ef4779a0a6d9
kthatoto <kthatoto@gmail.com>
2023-07-14 02:31:06 +0900

    Display result

--- src/modules/explore.ts
+++ src/modules/explore.ts
@@ -3,13 +3,20 @@ import { setTimeout } from "timers/promises";
 import { ITEMS } from "src/data/items";
 
 export const explore = async (page: Page) => {
+  const gettingItems = {};
   while (true) {
     await page.locator('.item-content.explorebtn').click();
     const resultItems = await page.locator('#exploreconsole > #consoletxt > img').all();
     for (const item of resultItems) {
       const img = await item.getAttribute('src');
       const itemInfo = ITEMS.find((i) => i.img === img);
-      if (itemInfo) console.log(itemInfo.name);
+      if (itemInfo) {
+        const itemName = itemInfo.name;
+        console.log(itemName);
+        if (!gettingItems[itemName]) gettingItems[itemName] = 0;
+        gettingItems[itemName]++;
+        console.log(JSON.stringify(gettingItems));
+      }
     }
     await setTimeout(5000);
   }
