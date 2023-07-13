42f566e196f86d7fbd3ef85544362cdc79067b87
kthatoto <kthatoto@gmail.com>
2023-07-14 02:48:52 +0900

    Remove file

--- src/play/debug.spec.ts
+++
@@ -1,3 +0,0 @@
-// https://zenn.dev/yusukeiwaki/articles/90bf05c2cf9a90
-// https://teratail.com/questions/u36g9ez1qcg8fj
-// https://playwright.dev/python/docs/api/class-browsertype?_highlight=launch_per#browser_typelaunch_persistent_contextuser_data_dir-kwargs
