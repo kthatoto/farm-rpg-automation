403ddefebaddb4630130bc31936e4b82e15ac59e
kthatoto <kthatoto@gmail.com>
2023-07-14 00:41:03 +0900

    Fix

--- src/modules/fish.ts
+++ src/modules/fish.ts
@@ -3,6 +3,7 @@ import { setTimeout } from "timers/promises";
 
 export const fish = async (page: Page) => {
   try {
+    let lastFishName = '';
     while (true) {
       await setTimeout(1000);
       const fishes = await page.locator('img.fish').all();
@@ -13,11 +14,14 @@ export const fish = async (page: Page) => {
           await page.locator('.picker-modal > .picker-modal-inner > .fishing-block > .fc').click({position: {x: 275, y: 25}});
           await setTimeout(100);
         }
-        await setTimeout(1000);
+        await setTimeout(200);
         const result = page.locator('#consoletxt').first();
-        const resultText = await result.innerText();
-        const fishName = resultText.split("\n");
-        console.log(fishName[fishName.length - 1]);
+        const resultText = await result.innerText({ timeout: 100 });
+        const resultTextArray = resultText.split("\n");
+        const fishName = resultTextArray[resultTextArray.length - 1]
+        if (fishName === lastFishName) return;
+        lastFishName = fishName;
+        console.log(fishName);
       });
     }
   } catch (e) {
