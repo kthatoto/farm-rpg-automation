3381f09e63df1744b6317c823ee39f4f425d0821
kthatoto <kthatoto@gmail.com>
2023-07-14 00:07:47 +0900

    Fix fishing

--- src/modules/fish.ts
+++ src/modules/fish.ts
@@ -2,19 +2,24 @@ import { type Page } from "@playwright/test";
 import { setTimeout } from "timers/promises";
 
 export const fish = async (page: Page) => {
-  while (true) {
-    await setTimeout(1000);
-    const fishes = await page.locator('img.fish').all();
-    fishes.forEach(async (fish) => {
-      if (!(await fish.isVisible())) return;
-      await fish.click({force: true});
-      for (const _ of new Array(30).fill(undefined)) {
-        await page.locator('.fishing-block > .fc').click({position: {x: 275, y: 25}});
-        await setTimeout(100);
-      }
+  try {
+    while (true) {
       await setTimeout(1000);
-      const result = await page.locator('#consoletxt').first().innerText();
-      console.log(result);
-    });
+      const fishes = await page.locator('img.fish').all();
+      fishes.forEach(async (fish) => {
+        if (!(await fish.isVisible())) return;
+        try { await fish.click({force: true}); } catch {}
+        for (const _ of new Array(30).fill(undefined)) {
+          await page.locator('.picker-modal > .picker-modal-inner > .fishing-block > .fc').click({position: {x: 275, y: 25}});
+          await setTimeout(100);
+        }
+        await setTimeout(1000);
+        const result = await page.locator('#consoletxt').first().innerText();
+        const fishName = result.split("\n");
+        console.log(fishName[fishName.length - 1]);
+      });
+    }
+  } catch (e) {
+    console.log(e);
   }
 };
