21150cf94e5478fa275fd10fa66875494b651e43
kthatoto <kthatoto@gmail.com>
2023-07-14 06:34:47 +0900

    Fix

--- src/play/farming/eggplant.spec.ts
+++ src/play/farming/eggplant.spec.ts
@@ -2,7 +2,7 @@ import { test } from '@playwright/test';
 import { goto } from '#modules/goto';
 import { farmingAll } from '#modules/farming';
 
-test('farming:potato', async ({page}) => {
+test('farming:eggplant', async ({page}) => {
   await goto(page, 'https://farmrpg.com/#!/xfarm.php?id=286431');
   await farmingAll(page, 'Eggplant');
 });
