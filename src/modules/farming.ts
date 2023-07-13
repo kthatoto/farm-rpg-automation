aa5ee291e44ce551f7a6d14f7297b9c70dfefa7f
kthatoto <kthatoto@gmail.com>
2023-07-14 06:36:43 +0900

    Fix

--- src/modules/farming.ts
+++ src/modules/farming.ts
@@ -8,7 +8,7 @@ export const farmingAll = async (page: Page, seedName: string) => {
     const seedInfo = SEEDS.find((s) => s.name === seedName);
     await page.getByRole('combobox').selectOption(seedInfo.id.toString());
     await page.getByRole('link', { name: 'Plant All Selected' }).click();
-    
-    await setTimeout(60 * 1000);
+
+    await setTimeout(20 * 1000);
   }
 };
