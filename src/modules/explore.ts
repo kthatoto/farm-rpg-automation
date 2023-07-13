e09a6fb388a83df4c3b53a439cc2d77c944ce379
kthatoto <kthatoto@gmail.com>
2023-07-14 02:24:28 +0900

    Display item on explore

--- src/modules/explore.ts
+++ src/modules/explore.ts
@@ -1,9 +1,16 @@
 import { type Page } from "@playwright/test";
 import { setTimeout } from "timers/promises";
+import { ITEMS } from "src/data/items";
 
 export const explore = async (page: Page) => {
   while (true) {
-    await setTimeout(5000);
     await page.locator('.item-content.explorebtn').click();
+    const resultItems = await page.locator('#exploreconsole > #consoletxt > img').all();
+    for (const item of resultItems) {
+      const img = await item.getAttribute('src');
+      const itemInfo = ITEMS.find((i) => i.img === img);
+      if (itemInfo) console.log(itemInfo.name);
+    }
+    await setTimeout(5000);
   }
 };
