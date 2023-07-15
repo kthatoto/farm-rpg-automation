6a8b4e768816e4be2951d8df7c782150b0d61938
kthatoto <kthatoto@gmail.com>
2023-07-15 19:56:52 +0900

    Add workshop to business

--- src/play/business.spec.ts
+++ src/play/business.spec.ts
@@ -7,6 +7,7 @@ import { nTimes } from '#utils/nTimes';
 import { buyMax } from '#modules/buyMax';
 import { farmingAllN } from '#modules/farming';
 import { sacrificeN } from '#modules/temple';
+import { craftAll } from '#modules/workshop';
 
 test('business', async ({ page }) => {
   while (true) {
@@ -27,5 +28,23 @@ test('business', async ({ page }) => {
     console.log('~~~~~Temple~~~~~')
     await sacrificeN(page, 'https://farmrpg.com/#!/templeitem.php?id=565', 1, 1000); // Hot Potato
     await sacrificeN(page, 'https://farmrpg.com/#!/templeitem.php?id=48', 1, 1000); // Potato
+
+    console.log('~~~~~Workshop~~~~~');
+    await goto(page, 'https://farmrpg.com/#!/workshop.php');
+
+    await craftAll(page, 'Unpolished Shimmer Stone');
+    await craftAll(page, 'Shimmer Stone');
+    await craftAll(page, 'Glass Orb');
+    await craftAll(page, 'Glass Bottle');
+
+    await craftAll(page, 'Iron Ring');
+    await craftAll(page, 'Salt');
+
+    await craftAll(page, 'Leather');
+    await craftAll(page, 'Sand');
+
+    await craftAll(page, 'Lantern');
+    await craftAll(page, 'Hourglass');
+    await craftAll(page, 'Fancy Pipe');
   }
 });
