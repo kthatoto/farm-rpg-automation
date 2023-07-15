0785c54a7b2a06430d17ee0860271a5c65700f54
kthatoto <kthatoto@gmail.com>
2023-07-15 19:58:20 +0900

    Revert "Fix"
    This reverts commit 54f24b6610bd1159edfd195e6b571e2dae4cba8f.

--- src/play/business.spec.ts
+++ src/play/business.spec.ts
@@ -1,6 +1,8 @@
 import { test } from '@playwright/test';
 import { goto } from '#modules/goto';
+import { setTimeout } from "timers/promises";
 import { getTime } from '#utils/timestamp';
+import { nTimes } from '#utils/nTimes';
 
 import { buyMax } from '#modules/buyMax';
 import { farmingAllN } from '#modules/farming';
@@ -9,12 +11,15 @@ import { craftAll } from '#modules/workshop';
 
 test('business', async ({ page }) => {
   while (true) {
-    await goto(page, 'https://farmrpg.com/index.php#!/store.php');
-    console.log('~~~~~Shopping~~~~~')
-    await buyMax(page, 'Potato Seeds');
-    await buyMax(page, 'Eggplant Seeds');
-    await buyMax(page, 'Worms');
-    console.log(getTime());
+    await nTimes(async () => {
+      await goto(page, 'https://farmrpg.com/index.php#!/store.php');
+      console.log('~~~~~Shopping~~~~~')
+      await buyMax(page, 'Potato Seeds');
+      await buyMax(page, 'Eggplant Seeds');
+      await buyMax(page, 'Worms');
+      console.log(getTime());
+      await setTimeout(6 * 1000);
+    }, 10);
 
     console.log('~~~~~Farming~~~~~')
     await goto(page, 'https://farmrpg.com/#!/xfarm.php?id=286431');
