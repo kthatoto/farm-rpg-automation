6895aa8f574434e32a3498da956db24374fa99e4
kthatoto <kthatoto@gmail.com>
2023-07-15 18:13:25 +0900

    Fix general explore

--- src/play/exploring/general.spec.ts
+++ src/play/exploring/general.spec.ts
@@ -9,23 +9,23 @@ test('exploring:general', async ({page}) => {
     await goto(page, 'https://farmrpg.com/#!/area.php?id=7');
     await exploreN(page, 'Forest', n, interval);
 
-    await goto(page, 'https://farmrpg.com/#!/area.php?id=1');
-    await exploreN(page, 'Small Cave', n, interval);
+    // await goto(page, 'https://farmrpg.com/#!/area.php?id=1');
+    // await exploreN(page, 'Small Cave', n, interval);
 
-    await goto(page, 'https://farmrpg.com/#!/area.php?id=2');
-    await exploreN(page, 'Small Spring', n, interval);
+    // await goto(page, 'https://farmrpg.com/#!/area.php?id=2');
+    // await exploreN(page, 'Small Spring', n, interval);
 
     await goto(page, 'https://farmrpg.com/#!/area.php?id=3');
     await exploreN(page, 'Highland Hills', n, interval);
 
-    await goto(page, 'https://farmrpg.com/#!/area.php?id=4');
-    await exploreN(page, 'Cane Pole Ridge', n, interval);
+    // await goto(page, 'https://farmrpg.com/#!/area.php?id=4');
+    // await exploreN(page, 'Cane Pole Ridge', n, interval);
 
-    await goto(page, 'https://farmrpg.com/#!/area.php?id=5');
-    await exploreN(page, 'Misty Forest', n, interval);
+    // await goto(page, 'https://farmrpg.com/#!/area.php?id=5');
+    // await exploreN(page, 'Misty Forest', n, interval);
 
-    await goto(page, 'https://farmrpg.com/#!/area.php?id=6');
-    await exploreN(page, 'Black Rock Canyon', n, interval);
+    // await goto(page, 'https://farmrpg.com/#!/area.php?id=6');
+    // await exploreN(page, 'Black Rock Canyon', n, interval);
 
     await goto(page, 'https://farmrpg.com/#!/area.php?id=8');
     await exploreN(page, 'Mount Banon', n, interval);
